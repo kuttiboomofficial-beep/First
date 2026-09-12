88184b82f1de8392ba6b059d9ded4e8474be33e9
kuttiboomofficial-beep <kuttiboomofficial@gmail.com>
2026-09-12 17:18:28 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-const CACHE='moneymate-v4.14';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(e.request.method==='GET' && (u.pathname==='/'||u.pathname.endsWith('/index.html'))){
-    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const CACHE='moneymate-v4-26';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
