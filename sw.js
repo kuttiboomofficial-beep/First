4cc82684c4836a46b9048b119aefb69b12e594be
kuttiboomofficial-beep <kuttiboomofficial@gmail.com>
2026-09-12 09:59:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='moneymate-v4.8';
+const CACHE='moneymate-v4.11';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
